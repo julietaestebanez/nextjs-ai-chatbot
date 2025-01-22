8010255f0a6768f3a141fc263c6b5706fe149998
Juan Tozzini <35992986-juansalvadortoz@users.noreply.replit.com>
2025-01-22 01:53:33 +0000

    cambio midlewere

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,19 @@
 import NextAuth from 'next-auth';
-
 import { authConfig } from '@/app/(auth)/auth.config';
 
 export default NextAuth(authConfig).auth;
 
+// Configuración de rutas protegidas por autenticación, excluyendo testSaveDocument
 export const config = {
-  matcher: ['/', '/:id', '/api/:path*', '/login', '/register'],
+  matcher: [
+    '/', 
+    '/:id', 
+    '/login', 
+    '/register', 
+    '/api/:path*',
+  ],
+  missing: [
+    { type: 'header', key: 'x-vercel-protection-bypass', value: '04863882959239923522127673998536' },
+    { type: 'header', key: 'x-api-key', value: process.env.API_KEY }
+  ]
 };
