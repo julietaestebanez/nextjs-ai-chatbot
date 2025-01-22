fb611fdc4725dc79a9e69b878d0ade241652ba77
Juan Tozzini <35992986-juansalvadortoz@users.noreply.replit.com>
2025-01-22 03:29:48 +0000

    middle que saltea testsave

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,15 @@
 import NextAuth from 'next-auth';
-
 import { authConfig } from '@/app/(auth)/auth.config';
 
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ['/', '/:id', '/api/:path*', '/login', '/register'],
+  matcher: [
+    '/',
+    '/:id',
+    // Aplica el middleware a cualquier ruta en /api excepto /api/testsavedocument
+    '/api/(?!testsavedocument).*',
+    '/login',
+    '/register',
+  ],
 };
