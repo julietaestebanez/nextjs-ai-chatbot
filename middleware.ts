45f562c99c41224868a7c4f2ac7a6432797968bc
Juan Tozzini <35992986-juansalvadortoz@users.noreply.replit.com>
2025-01-22 03:32:11 +0000

    vuelta atras middle

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,9 @@
 import NextAuth from 'next-auth';
+
 import { authConfig } from '@/app/(auth)/auth.config';
 
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: [
-    '/',
-    '/:id',
-    // Aplica el middleware a cualquier ruta en /api excepto /api/testsavedocument
-    '/api/(?!testsavedocument).*',
-    '/login',
-    '/register',
-  ],
+  matcher: ['/', '/:id', '/api/:path*', '/login', '/register'],
 };
