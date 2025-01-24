03028c4f291264510bfc7e9130b74bba44a61de4
Juan Tozzini <35992986-juansalvadortoz@users.noreply.replit.com>
2025-01-24 03:08:46 +0000

    cambios en cors

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,18 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },  // Permitir solicitudes desde cualquier origen
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },  // Métodos permitidos
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },  // Encabezados permitidos
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
